cc9bea9ffdea1311b7c9060097e5635244a2b7ac
Brian <mayrose.dg@gmail.com>
2018-01-26 21:21:31 -0800

    2nd

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,4 +1,4 @@
-"""movie_app URL Configuration
+"""blog_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
@@ -29,5 +29,3 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
